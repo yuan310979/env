b40efc2b46ff4aa5c60bdf9662ecc28542a6664c
yuan310979 <chc71340@gmail.com>
2018-12-19 02:52:30 +0800

    bug fix

--- ror.sh
+++ ror.sh
@@ -1,5 +1,4 @@
-sudo apt-get install -y libreadline-dev
-sudo apt-get install ruby-all-dev
+sudo apt-get install -y libreadline-dev libssl-dev ruby-all-dev
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
